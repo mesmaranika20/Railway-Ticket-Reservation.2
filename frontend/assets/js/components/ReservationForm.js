7a3fe2035ae7829f30b2fe5e644749ecfc638a19
Smaranika Sahu <smaranika.s2006@gmail.com>
2026-01-17 16:41:27 +0530

    Add staff dropdown to reservation form

--- frontend/assets/js/components/ReservationForm.js
+++ frontend/assets/js/components/ReservationForm.js
@@ -1,11 +1,13 @@
 import { $ } from "../utils/dom.js";
 
-export function fillReservationDropdowns(trains, bookings) {
+export function fillReservationDropdowns(trains, bookings, staff) {
   const trainSel = $("train_id");
   const bookingSel = $("booking_id");
+  const staffSel = $("staff_id");
 
   trainSel.innerHTML = `<option value="">Select train</option>`;
   bookingSel.innerHTML = `<option value="">Select booking</option>`;
+  staffSel.innerHTML = `<option value="">Select staff</option>`;
 
   (trains || []).forEach(t => {
     const opt = document.createElement("option");
@@ -18,6 +20,13 @@ export function fillReservationDropdowns(trains, bookings) {
     const opt = document.createElement("option");
     opt.value = b.id;
     opt.textContent = `${b.passenger_name} (ID: ${b.id})`;
+    bookingSel.appendChild(opt);
+  });
+
+   (staff || []).forEach(b => {
+    const opt = document.createElement("option");
+    opt.value = s.id;
+    opt.textContent = `${s.name} (ID: ${s.id})`;
     bookingSel.appendChild(opt);
   });
 }
