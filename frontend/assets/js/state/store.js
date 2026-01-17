6e2b3cff4edf1ef6561c290b2995f90291d5932a
Smaranika Sahu <smaranika.s2006@gmail.com>
2026-01-17 16:52:03 +0530

    Remove staffs from global app state
    Remove staffs array from global app state.

--- frontend/assets/js/state/store.js
+++ frontend/assets/js/state/store.js
@@ -2,7 +2,7 @@
 let state = {
   editingId: null,   // which student is being edited
   trains: [],       // list of all trains
-  staffs: []       // list of all staffs
+
 };
 
 // Update part of the state
