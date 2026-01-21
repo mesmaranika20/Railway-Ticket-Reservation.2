ab09b8b8aef17f34b22a5f9fa659a817d3bfa42a
Smaranika Sahu <smaranika.s2006@gmail.com>
2026-01-21 04:32:11 +0000

    add data for ticket

--- database/report.py
+++ database/report.py
@@ -21,12 +21,12 @@ def reservation_report():
                         
             b.id AS booking_id,
             b.passenger_name AS booking_passenger_name,
-            b.coach AS booking_coach_number,
-            b.booking AS booking_booking_date,            
+            b.coach_number AS booking_coach_number,
+            b.booking_date AS booking_booking_date,
             b.total_seats AS booking_total_seats,
             b.payment AS booking_payment,
                         
-            s.name AS staff_name,
+             s.name AS staff_name,
             s.role AS staff_role,
             s.contact AS staff_contact      
                         
